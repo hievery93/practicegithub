267b01f5b724311ba84dce1dcba62d642437c872
sys <akrrktlejf@gmail.com>
2022-11-27 01:00:43 +0900

    base

--- login.py
+++ login.py
@@ -1,5 +1,9 @@
 
 
-def (id, pw) : 
+def input_login(id, pw) : 
     if id == 1 and pw == 2 : 
         print("로그인 되었습니다 ")
+
+
+def delete ( ) : 
+    print("지우는 함수")
---
+++ view.py
@@ -0,0 +1,3 @@
+
+def view_success ( ) : 
+    print( "로그인이 성공적으로 되면 애니메이션 띄움")
